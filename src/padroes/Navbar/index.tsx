23b2eae2af22eb1a791bcf62f4798e69523e9db2
artphil <artphil.dev@gmail.com>
2022-09-08 15:34:34 -0300

    inserindo estilo css

--- src/padroes/Navbar/index.tsx
+++ src/padroes/Navbar/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useUsuarioLogado } from "../../estados/hooks/usuario";
 
+import estilos from './Navbar.module.css'
+
 function Navbar() {
 	const [mostraMenu, setMostraMenu] = useState(false);
 	const usuario = useUsuarioLogado();
@@ -8,31 +10,42 @@ function Navbar() {
 		setMostraMenu(!mostraMenu)
 	}
 	return (
-		<section>
-			<nav>
-				<button
-					title="Menu"
-					onClick={toggleMenu}
-				>
-					Menu
-				</button>
+		<div className={estilos.navbar}>
+			<nav className={estilos.navbarContent}>
+
+				<div className={estilos.navbarBotao}>
+					<button
+						title="Menu"
+						onClick={toggleMenu}
+					>
+						Menu
+					</button>
+				</div>
+
+				<div className={estilos.navbarTitulo}>
+					<h1>SAO | Sistema de Apoio a Operação</h1>
+				</div>
+
 				<h1>{usuario?.nome}</h1>
-				{!usuario &&
-					<button>Entrar</button>
-				}
-				{usuario &&
-					<button>Sair</button>
-				}
+
+				<div className={estilos.navbarBotao}>
+					{!usuario &&
+						<button>Entrar</button>
+					}
+					{usuario &&
+						<button>Sair</button>
+					}
+				</div>
 			</nav>
 			{mostraMenu &&
-				<div>
+				<div className={estilos.menu}>
 					<ul>
 						<li>
 							item menu
 						</li>
 					</ul>
 				</div>}
-		</section>
+		</div>
 	);
 }
 
