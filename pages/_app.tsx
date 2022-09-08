98b5769efd88ea241222c6fb29e1c7c48a02f404
artphil <artphil.dev@gmail.com>
2022-09-08 14:37:47 -0300

    Configurando titulo da aba

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
+			<Head>
+				<title>SAO | Sistema de Apoio a Operação</title>
+				<link rel="icon" href="/sao_logo.svg" />
+			</Head>
 			<Component {...pageProps} />
 		</RecoilRoot>
 	);
